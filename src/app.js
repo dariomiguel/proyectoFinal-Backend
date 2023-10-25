5f3d54541fed50b2a006d4abb2623370b74b2beb
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-10-24 22:40:03 -0300

    Agregar ejemplo visto en clase para trabajar desde ahí

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import routerPets from "./router/pets.router.js"
 
 const app = express();
-
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+//Agregamos la propiedad de Express para poder seleccionar la carpeta public
+app.use("/static", express.static("../src/public"))
 app.get("/", async (req, res) => res.json("OK!"))
+
+app.use("/api/pets", routerPets)
 
 app.listen(8080, () => console.log("En linea..."))
