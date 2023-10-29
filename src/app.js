45510b78cd2128e53eab1ec4ac8bd1370b3c2dfa
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-10-29 10:41:17 -0300

    Configurar en app.js el motor de plantillas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 import routerProducts from "./router/products.router.js"
 import routerCarts from "./router/carts.router.js"
+import __dirname from "./utils.js"
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+//Configuramos el motor de plantillas
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "views");
+app.set("view engine", "handlebars");
+
 //Agregamos la propiedad de Express para poder seleccionar la carpeta public
-app.use("/static", express.static("../src/public"))
+app.use(express.static(__dirname + "/public"))
 app.get("/", async (req, res) => res.json("OK!"))
 
 app.use("/api/products", routerProducts);
