3a412728d8c1d82dd9dd22a6c2aaa5e969ffc6f4
Maira netbook <maira.v.coria@gmail.com>
2023-11-03 21:32:46 -0300

    Agregar estilos css

--- src/router/realTimeProducts.router.js
+++ src/router/realTimeProducts.router.js
@@ -22,6 +22,7 @@ router.get("/", async (req, res) => {
             }
         }
         res.render("realtimeproducts", {
+            style: "realTimeProducts.css",
             products
         });
     } catch (error) {
