5d59ba1a5ed181bb066a72643bbd2eec59f184f5
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-11-26 22:16:36 -0300

    Products: Se colocó la lígica de los botones previous/Next para que no sean visibles cuando no exista en el campo y se agregó el limit por input

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,1 +1,17 @@
-// const socket = io();
+document.querySelector("#btnPrev").onclick = () => {
+    const prevPage = document.querySelector("#prevPage").value;
+    const limit = document.querySelector("#limit").value;
+
+    const url = `/?page=${prevPage}&limit=${limit}`;
+
+    document.location.href = url;
+}
+
+document.querySelector("#btnNext").onclick = () => {
+    const nextPage = document.querySelector("#nextPage").value;
+    const limit = document.querySelector("#limit").value;
+
+    const url = `/?page=${nextPage}&limit=${limit}`;
+
+    document.location.href = url;
+}
