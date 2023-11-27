f8cb037696eee40bc3344f1db7917995d556d5f2
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-11-27 20:15:53 -0300

    Refactorización de 'home.js'

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,32 +1,32 @@
+// Función para construir la URL y redireccionar
+const redirectToPage = (page, limit, query, category) => {
+    const url = `/?page=${page}&limit=${limit}&query=${query}&category=${category}`;
+    document.location.href = url;
+};
+
+// Evento para el botón "Prev"
 document.querySelector("#btnPrev").onclick = () => {
     const prevPage = document.querySelector("#prevPage").value;
     const limit = document.querySelector("#limit").value;
     const query = document.querySelector("#query").value;
     const category = document.querySelector("#category").value;
+    redirectToPage(prevPage, limit, query, category);
+};
 
-    const url = `/?page=${prevPage}&limit=${limit}&query=${query}&category=${category}`;
-
-    document.location.href = url;
-}
-
+// Evento para el botón "Next"
 document.querySelector("#btnNext").onclick = () => {
     const nextPage = document.querySelector("#nextPage").value;
     const limit = document.querySelector("#limit").value;
     const query = document.querySelector("#query").value;
     const category = document.querySelector("#category").value;
+    redirectToPage(nextPage, limit, query, category);
+};
 
-    const url = `/?page=${nextPage}&limit=${limit}&query=${query}&category=${category}`;
-
-    document.location.href = url;
-}
-
+// Evento para el botón "Search"
 document.querySelector("#btnSearch").onclick = () => {
     const nextPage = document.querySelector("#page").value;
     const limit = document.querySelector("#limit").value;
     const query = document.querySelector("#query").value;
     const category = document.querySelector("#category").value;
-
-    const url = `/?page=${nextPage}&limit=${limit}&query=${query}&category=${category}`;
-
-    document.location.href = url;
-}
+    redirectToPage(nextPage, limit, query, category);
+};
